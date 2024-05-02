714a2b555f826ed59ae320cc3390e656b1b31ad5
Ali <46187027+aliakeel@users.noreply.github.com>
2024-05-02 11:08:00 +0300

    get new endpoint weather 2

--- TestWebApplication/Controllers/WeatherForecastController.cs
+++ TestWebApplication/Controllers/WeatherForecastController.cs
@@ -18,6 +18,18 @@ namespace TestWebApplication.Controllers
             _logger = logger;
         }
 
+        [HttpGet(Name = "GetWeatherForecast2")]
+        public IEnumerable<WeatherForecast> Get2()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
